66ed78b63470947f7e12e2f35f9a6722bc4d04e7
nermin.vkl@gmail.com <nermin.vkl@gmail.com>
2023-04-01 22:07:35 +0200

    small changes

--- public/index.search.js
+++ public/index.search.js
@@ -3,7 +3,7 @@ var relearn_search_index = [
     "content": "Pregled službenih glasnika Općine Velika Kladuša Godišnja pretplata za štampanu verziju Službenog glasnika iznosi 50KM a plaća se unaprijed kao akontacija. Konačni iznos pretplate utvrđuje se izlaskom posljednjeg broja Službenog glasnika na kraju godine.\nPretplata se vrši na depozitni račun Općinskog organa uprave Općine Velika Kladuša broj: 3380002211423047 otvoren kod UniCredit banke dd Mostar, vrsta prihoda 723139, poziv na broj 0000000000. Šifra općine Velika Kladuša je: 097.\n",
     "description": "",
     "tags": null,
-    "title": "Službeni glasnici",
+    "title": "Službeni glasnici 2023. godina",
     "uri": "/slglasnici/index.html"
   },
   {
@@ -35,18 +35,25 @@ var relearn_search_index = [
     "uri": "/index.html"
   },
   {
-    "content": "Pregled službenih glasnika po godinama Da bi izvršili pregled glasnika molimo Vas da izaberete željenu godinu:\n2023 2022 2021 2020\n",
+    "content": "Pregled službenih glasnika po izdanjima Da bi izvršili pregled glasnika molimo Vas da izaberete željenu godinu:\n2023 2022 2021 2020\n",
     "description": "",
     "tags": null,
-    "title": "Službeni glasnici po godinama",
+    "title": "Službeni glasnici po izdanjima",
     "uri": "/slglasnici/2023/index.html"
   },
   {
-    "content": "Pregled službenog glasnika, broj 1/2023 ",
+    "content": "Pregled službenog glasnika, broj 1/2023 Kliknite ovdje da preuzmete glasnik u PDF Sadržaj Općinsko vijeće Rb Tačka Stranica 1. Zaključak o nepotvrđivanju Zaključaka Općinskog vijeća Općine Velika Kladuša o neusvajanju prijedloga Programa zajedničke komunalne potrošnje Općine Velika Kladuša za 2023. godinu, broj: 01-04-321/22 od 20.12.2022. godine 1 2. Zaključak o nepotvrđivanju Zaključaka Općinskog vijeća Općine Velika Kladuša o neusvajanju prijedloga Odluke o ovlaštenju organizacije za davanje stručne ocjene u postupku izdavanja urbanističke saglasnosti broj: 01-04-319/22 od 20.12.2022. godine 28 3. Zaključak o nepotvrđivanju Zaključaka Općinskog vijeća Općine Velika Kladuša o neusvajanju Prijedloga Odluke o privremenom finansiranju Općine Velika Kladuša za 2023. godinu broj: 01-04-5/23 od 12.01.2023. godine 29 4. Zaključak o nepotvrđivanju Zaključaka Općinskog vijeća Općine Velika Kladuša o neusvajanju Nacrta Budžeta Općine Velika Kladuša za 2023. godinu, broj: 01-04-318/22 od 20.12.2022. godine 31 5. Zaključak o nepotvrđivanju Odluke o izboru Komisije za izbor i imenovanje, broj: 01-04-19/23 od 30.01.2023. godine 32 6. Zaključak o nepotvrđivanju Odluke o izboru Predsjedavajućeg Općinskog vijeća Općine Velika Kladuša, broj: 01-04-20/23 od 30.01.2023. godine 33 Općinski načelnik Rb Tačka Stranica 7. Odluke o isknjižavanju imovine x3 33-34 8. Odluka o prihvatanju donacije 34 9. Pravilnik o izradi, upotrebi, čuvanju i uništavanju pečata u organu uprave Općine Velika Kladuša 35 10. Zaključci o dodjeli i isplati sredstava iz budžeta Općine Velika Kladuša 35-66 ",
     "description": "",
     "tags": null,
     "title": "Službeni glasnik broj 1/2023",
     "uri": "/slglasnici/2023/1/index.html"
+  },
+  {
+    "content": "Pregled službenog glasnika, broj 2/2023 Kliknite ovdje da preuzmete glasnik u PDF Sadržaj Općinsko vijeće Rb Tačka Stranica 1. Rješenje o ne usvajanju Prijedloga Odluke o utvrđivanju javnog interesa „Mrcelji 1 1 2. Zaključak Parlamentu i Vladi F BiH a na osnovu presude Evropskog suda za ljudska prava u predmetu Murtić i Ćerimović protiv BiH 1 3. Zaključak o traženju od Općinskog načelnika materijala za tematske sjednice Strategija i razvoj Općine 2 4. Zaključak o ne usvajanju Prijedloga Odluke I fin. Izvještaja 01.01-30.06.2020. godine 2 5. Zaključak o ne usvajanju Prijedloga Odluke I fin. Izvještaja 01.01-30.09.2020. godine 2 6. Zaključak o ne usvajanju Prijedloga Odluke I fin. Izvještaja 01.01-30.06.2021. godine 2 7. Zaključak o ne usvajanju Prijedloga Odluke I fin. Izvještaja 01.01-30.09.2021. godine 3 8. Zaključak o prihvatanju i upućivanju na Javnu raspravu Nacrta Budžeta za 2022. godinu 3 9. Zaključak o prihvatanju i upućivanju na Javnu raspravuNacrta Odluke o komunalnim taksama i tarifi komunalnih taksa 3 10. Zaključak o ne usvajanju Prijedloga Odluke o privremenom finansiranju za 2022. godinu 3 11. Zaključak o ne usvajanju Prijedloga Programa zajedničke komunalne potrošnje za 2022. godine 4 Općinski načelnik Rb Tačka Stranica 12. Odluke o isknjižavanju imovine x2 4 13. Zaključci o dodjeli sredstava iz budžeta Općine Velika Kladuša 4-21 ",
+    "description": "",
+    "tags": null,
+    "title": "Službeni glasnik broj 2/2023",
+    "uri": "/slglasnici/2023/2/index.html"
   },
   {
     "content": "",
